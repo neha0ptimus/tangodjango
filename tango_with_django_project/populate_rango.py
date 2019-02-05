fab657739c94e916b456dc999e17607c1c3deda3
Neha Bora <nehabora@0ptiadmins-MacBook-Pro-3.local>
2019-02-05 13:32:37 -0500

    finished chapter 6

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -22,45 +22,54 @@ def populate():
     python_pages = [
         {
             'title': "Official Python Tutorial",
-            'url': "http://docs.python.org/2/tutorial/"
+            'url': "http://docs.python.org/2/tutorial/",
+            'views': 23,
         },
         {
             'title': "How to Think like a Computer Scientist",
-            'url': "http://www.greenteapress.com/thinkpython/"
+            'url': "http://www.greenteapress.com/thinkpython/",
+            'views': 34,
         },
         { 
             'title': "Learn Python in 10 Minutes",
-            'url': "http://www.korokithakis.net/tutorials/python/"
+            'url': "http://www.korokithakis.net/tutorials/python/",
+            'views': 45,
         }
     ]
 
     django_pages = [
         {
             'title': 'Official Django Tutorial',
-            'url': 'https://docs.djangoproject.com/en/1.9/intro/tutorial01/'
+            'url': 'https://docs.djangoproject.com/en/1.9/intro/tutorial01/',
+            'views': 56,
         },
         {
             'title': "Django Rocks",
-            'url': "http://www.djangorocks.com/"
+            'url': "http://www.djangorocks.com/",
+            'views': 67,
         },
         {
             'title': "How to Tango with Django",
-            'url': "http://www.tangowithdjango.com/"
+            'url': "http://www.tangowithdjango.com/",
+            'views': 78,
         }
     ]
 
     other_pages = [
         {
             'title': "Bottle",
-            'url': "http://bottlepy.org/docs/dev/"
+            'url': "http://bottlepy.org/docs/dev/",
+            'views': 89,
         },
         {
             'title': "Flask",
-            'url': "http://flask.pocoo.org"
+            'url': "http://flask.pocoo.org",
+            'views': 90,
         },
         {
             'title': "Spark",
-            'url': 'https://spark.apache.org/docs/2.4.0/'
+            'url': 'https://spark.apache.org/docs/2.4.0/',
+            'views': 33,
         }
     ]
 
@@ -87,7 +96,7 @@ def populate():
         c = add_cat(cat, cat_data['views'], cat_data['likes'])
         
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
 
 
     for c in Category.objects.all():
